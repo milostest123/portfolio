8440703d04fb0df1acf304ceda04753ec67c4849
milostest123 <miloscirkovictest@gmail.com>
2022-02-14 14:53:07 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './start-page/contact/contact.component';
 import { FooterComponent } from './start-page/footer/footer.component';
 import { HeaderComponent } from './start-page/header/header.component';
-import { HeroComponent } from './start-page/hero/hero.component';
 import { NavbarComponent } from './start-page/navbar/navbar.component';
 import { ProjectsComponent } from './start-page/projects/projects.component';
 import {StartPageComponent}  from './start-page/start-page.component'
@@ -17,11 +18,13 @@ import {StartPageComponent}  from './start-page/start-page.component'
     HeaderComponent,
     StartPageComponent,
     ProjectsComponent,
-    HeroComponent,
     ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/start-page/contact/contact.component.ts
+++ src/app/start-page/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  contForm!:FormGroup
+  send:boolean=false;
+  messageEmail:string='';
+  constructor(
+    private formBuilder:FormBuilder,
+    private _http:HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.contForm=this.formBuilder.group({
+        from:['',Validators.required],
+        subject:['',Validators.required],
+        message:['', Validators.required]
+    })
+  }
+
+  sendEmail(){
+    this._http.post<any>("http://236p122.mars1.mars-hosting.com/api/auth/email",this.contForm.value).subscribe(res=>{
+      if(res){
+        this.messageEmail='You have successfully sent an email';
+        setTimeout(() => {
+          this.messageEmail='';
+        }, 4000);
+        this.contForm.reset();
+      }else{
+        this.messageEmail='Bad request!!'
+       setTimeout(() => {
+          this.messageEmail='';
+       }, 4000);
+        
+      }
+    },
+    err=>{
+      console.log(err);
+      
+    }
+    )
   }
 
 }
